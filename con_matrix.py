807bf77c657082c3871d7502311d94e52cde2eca
OzupeSir <51609801+OzupeSir@users.noreply.github.com>
2019-09-03 20:55:26 +0800

    Update con_matrix.py

--- con_matrix.py
+++ con_matrix.py
@@ -7,11 +7,17 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 
 
-# In[ ]:
-
 
 def con_matrix(Y_test,y_hat):
-    print('accuracy:',accuracy_score(Y_test,y_hat))
+    '''
+    func:输出矩阵的召回率以及精确度，并且构造混淆矩阵的图像
+    
+    paramas: 
+    Y_test:输入Y的实际值
+    y_hat：输入Y的预测值
+    
+    '''
+    print('recall:',recall_score(Y_test,y_hat))
     print('precision:',precision_score(Y_test,y_hat))
     cm = confusion_matrix(Y_test,y_hat,labels=[1,0])
     print('confusion matrix',cm)
